0e2a9a0a95f94acc3f53fa539da224dc49b91abc
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-02-04 00:05:06 +0100

    Use Dagger Hilt in MainActivity and HomeViewModel

--- app/src/main/java/com/danipl/frameworkguesser/MainActivity.kt
+++ app/src/main/java/com/danipl/frameworkguesser/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.danipl.frameworkguesser.ui.theme.FrameworkGuesserTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
