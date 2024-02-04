42098197b6b44d506aa68738a56f117967ec066a
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-02-04 13:06:48 +0100

    Create GetReactNativeAppsUseCase

--- app/src/main/java/com/danipl/frameworkguesser/domain/GetReactNativeAppsUseCase.kt
+++ app/src/main/java/com/danipl/frameworkguesser/domain/GetReactNativeAppsUseCase.kt
@@ -1,10 +1,15 @@
 package com.danipl.frameworkguesser.domain
 
 import com.danipl.frameworkguesser.data.AdbRepository
+import javax.inject.Inject
 
 /**
  * This use case fetches the apps that use react native in the user's phone.
  */
-class GetReactNativeAppsUseCase(
-    private val AdbRepository: AdbRepository
-)
+class GetReactNativeAppsUseCase @Inject constructor(
+    private val adbRepository: AdbRepository
+) {
+    fun getReactNativeApps(): List<String> {
+        return adbRepository.getReactNativeApps()
+    }
+}
