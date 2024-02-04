978ed46beb8c2385a06c3a5709a34d91b7127b31
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-02-04 13:04:32 +0100

    Create AppComponent with an Injectable appContext

---
+++ app/src/main/java/com/danipl/frameworkguesser/AppComponent.kt
@@ -0,0 +1,16 @@
+package com.danipl.frameworkguesser
+
+import android.content.Context
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+
+@InstallIn(SingletonComponent::class)
+@EntryPoint
+interface AppComponent {
+
+    @get:ApplicationContext
+    val appContext: Context
+}
