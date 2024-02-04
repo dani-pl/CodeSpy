b7e5f701135fab9eac23ae847790e29691fa5fb4
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-02-04 13:06:03 +0100

    Use packageManager in AdbRepository to get a list of the react native apps installed in a device

--- app/src/main/java/com/danipl/frameworkguesser/data/AdbRepository.kt
+++ app/src/main/java/com/danipl/frameworkguesser/data/AdbRepository.kt
@@ -1,7 +1,34 @@
 package com.danipl.frameworkguesser.data
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class AdbRepository @Inject constructor() {
+class AdbRepository @Inject constructor(
+    @ApplicationContext private val appContext: Context
+) {
 
+    private val pm = appContext.packageManager
+
+    fun getReactNativeApps(): List<String> {
+        val installedReactNativeApps = mutableListOf<String>()
+        pm.getInstalledApplications(0).forEach {
+            if (isReactNativeApp(it.packageName)){
+                installedReactNativeApps.add(it.loadLabel(pm).toString())
+            }
+        }
+        return installedReactNativeApps
+    }
+
+    private fun isReactNativeApp(packageName: String): Boolean {
+        return try {
+            pm
+                .getResourcesForApplication(packageName)
+                .assets
+                .open("index.android.bundle")
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
 }
