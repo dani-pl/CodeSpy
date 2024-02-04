2f21ff5df3aaac10034ec5fec3d26e69f6ecd705
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-02-04 13:07:53 +0100

    Use GetReactNativeAppsUseCase in HomeViewModel and expose the list of apps in a state

--- app/src/main/java/com/danipl/frameworkguesser/ui/HomeViewModel.kt
+++ app/src/main/java/com/danipl/frameworkguesser/ui/HomeViewModel.kt
@@ -1,13 +1,17 @@
 package com.danipl.frameworkguesser.ui
 
 import androidx.lifecycle.ViewModel
+import com.danipl.frameworkguesser.domain.GetReactNativeAppsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getReactNativeAppsUseCase: GetReactNativeAppsUseCase
+) : ViewModel() {
     private val _state = MutableStateFlow(
         HomeState(
             appName = "Outlook",
@@ -15,6 +19,14 @@ class HomeViewModel @Inject constructor() : ViewModel() {
         )
     )
     val state = _state.asStateFlow()
+
+    fun getReactNativeApps(){
+        _state.update {
+            it.copy(
+                appName = getReactNativeAppsUseCase.getReactNativeApps().toString()
+            )
+        }
+    }
 }
 
 data class HomeState(val appName: String, val isReactNativeApp: Boolean)
