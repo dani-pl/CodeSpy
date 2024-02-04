768241704a0176bbc208c4a507d37f4d5fdf407a
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-02-04 13:52:37 +0100

    Create AppInfoDataModel

---
+++ app/src/main/java/com/danipl/frameworkguesser/data/models/DataModels.kt
@@ -0,0 +1,9 @@
+package com.danipl.frameworkguesser.data.models
+
+import android.graphics.drawable.Drawable
+
+data class AppInfoDataModel(
+    val name: String,
+    val packageName: String,
+    val icon: Drawable,
+)
