17e0c498f406ec50fcc614b4642b720800fb462f
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-10-13 17:39:00 +0200

    Add @Provides function for AppDatabase

--- app/src/main/java/com/danipl/codespy/AppComponent.kt
+++ app/src/main/java/com/danipl/codespy/AppComponent.kt
@@ -1,10 +1,15 @@
 package com.danipl.codespy
 
 import android.content.Context
+import androidx.room.Room
+import com.danipl.codespy.data.db.AppDatabase
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @InstallIn(SingletonComponent::class)
@@ -13,4 +18,18 @@ interface AppComponent {
 
     @get:ApplicationContext
     val appContext: Context
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesAppDatabase(
+        @ApplicationContext appContext: Context
+    ): AppDatabase = Room.databaseBuilder(
+        appContext,
+        AppDatabase::class.java, "app-database"
+    ).build()
 }
