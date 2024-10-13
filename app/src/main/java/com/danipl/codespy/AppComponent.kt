2e047cf719b54ad961bdaa3cc6029c021cd63f1b
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-10-13 21:34:13 +0200

    Add CoroutineQualifiers

--- app/src/main/java/com/danipl/codespy/AppComponent.kt
+++ app/src/main/java/com/danipl/codespy/AppComponent.kt
@@ -3,12 +3,15 @@ package com.danipl.codespy
 import android.content.Context
 import androidx.room.Room
 import com.danipl.codespy.data.db.AppDatabase
+import com.danipl.codespy.util.IoDispatcher
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
 
@@ -32,4 +35,8 @@ object AppModule {
         appContext,
         AppDatabase::class.java, "app-database"
     ).build()
+
+    @Provides
+    @IoDispatcher
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
