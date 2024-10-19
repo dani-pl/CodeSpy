109b359bf32682803fd416ab4693d48696982952
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-10-19 19:15:11 +0200

    Add LoadingScreen

---
+++ app/src/main/java/com/danipl/codespy/ui/LoadingScreen.kt
@@ -0,0 +1,36 @@
+package com.danipl.codespy.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen(
+    modifier: Modifier = Modifier,
+    message: String
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(text = message, textAlign = TextAlign.Center)
+    }
+
+}
