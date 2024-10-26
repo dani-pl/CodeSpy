06422856c65cc1753448e184d421aae2b717ef19
Daniel Pena Lopez <danielpenalopez15@gmail.com>
2024-10-26 20:58:08 +0200

    Use waiting_with_coffee image in LoadingScreen

--- app/src/main/java/com/danipl/codespy/ui/LoadingScreen.kt
+++ app/src/main/java/com/danipl/codespy/ui/LoadingScreen.kt
@@ -1,18 +1,22 @@
 package com.danipl.codespy.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.danipl.codespy.R
 
 @Composable
 fun LoadingScreen(
@@ -26,11 +30,19 @@ fun LoadingScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Image(
+            modifier = Modifier.requiredHeight(300.dp),
+            painter = painterResource(id = R.drawable.waiting_with_coffee),
+            contentDescription = ""
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
         CircularProgressIndicator()
 
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(modifier = Modifier.height(15.dp))
 
         Text(text = message, textAlign = TextAlign.Center)
     }
-
 }
